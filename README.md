98d15a692b80520d012c2bade45318fd51501a31
hackru <hackru@gmail.com>
2020-04-15 01:32:25 +0300

    Initial import

---
+++ README.md
@@ -0,0 +1,28 @@
+# Simple nodejs key-value http server with TTL support
+Used mainly for catching remote iiot devices keepalives.
+Expected to run on extreme resources-limited container such as heroku.
+Keys not updated in TTL interval will be deleted from keystore.
+
+#### Install
+Install prereqs:
+```npm install```
+#### Usage
+Run:
+```node index.js```
+ENV variables:
+* PORT (default 8080) // 0 for random unused port
+* TTL (default 12000) // 2 minutes
+* DEBUG (default 0) // no debug messages in console
+* BIND (default 127.0.0.1) // bind to localhost
+* MAXSIZE (default 100) // keeping last 100 keys in memory, 0 for unlimited
+
+Run with parameters:
+```PORT=1234 TTL=60000 DEBUG=1 BIND=0.0.0.0 MAXSIZE=200 node index.js```
+
+#### Set key to value
+* HTTP GET /?key=value
+#### Get key value:
+* HTTP GET /?key
+
+Originally written by Ivan Void, modded by @hackruu:
+addded TTL, DEBUG, BIND, MAXSIZE options.
---
+++ index.js
@@ -0,0 +1,36 @@
+const Keyv = require("keyv");
+const express = require("express");
+const QuickLRU = require('quick-lru');
+
+const port = process.env.PORT || 8080;
+const bind = process.env.BIND || '127.0.0.1'
+const ttl = Number(process.env.TTL) || 12000; // 120 seconds
+const debug = Number(process.env.DEBUG) || 0;
+const maxsize = Number(process.env.MAXSIZE) || 100;
+
+const app = express();
+const lru = new QuickLRU({ maxSize: maxsize });
+const keyv = new Keyv({ store: lru });
+
+app.use(express.json());
+
+app.all("*", async (req, res) => {
+  if ( debug == 1 ) { console.warn(req.query); };
+  const reqObject = req.query;
+  const firstObjectKey =
+    Object.keys(reqObject).length && Object.keys(reqObject)[0];
+  const firstObjectValue = reqObject[firstObjectKey];
+
+  if (firstObjectKey && firstObjectValue === "") {
+    const value = await keyv.get(firstObjectKey);
+    return value ? res.send(value) : res.sendStatus(404);
+  }
+
+  await keyv.set(firstObjectKey, firstObjectValue, ttl);
+  return res.sendStatus(200);
+});
+
+app.listen(port, bind , () => {
+  if ( debug == 1 ) { console.warn("Debug is enabled!"); };
+  console.log(`App listening on ${bind}:${port}, ttl is ${ttl}, maxsize is ${maxsize}!`);
+});
